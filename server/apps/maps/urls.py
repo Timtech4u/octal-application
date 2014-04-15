c5e29e3b9312d9b89fcbd3aa5f11a6bd29c9fcb4
Dan Armendariz <danallan@cs.berkeley.edu>
2014-04-14 19:00:55 -0700

    ensure map ID is provided

--- server/apps/maps/urls.py
+++ server/apps/maps/urls.py
@@ -5,7 +5,7 @@ from apps.maps import views
 from apps.ki.views import knowledge_inference
 
 urlpatterns = patterns('',
-                       url(r'^(?P<gid>[0-9]*)', include(patterns('',
+                       url(r'^(?P<gid>[0-9]+)', include(patterns('',
                            url(r'^/build$', views.build, name='build'),
                            url(r'^/exercises/', include('apps.exercises.urls', namespace="exs")),
                            url(r'^/ki', knowledge_inference, name="ki"),
