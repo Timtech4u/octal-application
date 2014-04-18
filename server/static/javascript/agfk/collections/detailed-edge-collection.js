2bbd3a83edb0370e57b85dc4d6cb40de41b9b4f9
Dan Armendariz <danallan@cs.berkeley.edu>
2014-04-17 18:27:32 -0700

    remove agfk backbone app

--- server/static/javascript/agfk/collections/detailed-edge-collection.js
+++
@@ -1,6 +0,0 @@
-/*global define*/
-define(["backbone", "lib/kmapjs/collections/edge-collection", "agfk/models/detailed-edge-model"], function(Backbone, EdgeCollection, DetailedEdgeModel){
-  return  EdgeCollection.extend({
-    model: DetailedEdgeModel
-  });
-});
