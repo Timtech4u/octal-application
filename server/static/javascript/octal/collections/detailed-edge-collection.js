749501b6c30bc14cc67a58bcdb60bc6a60ed8364
Dan Armendariz <danallan@cs.berkeley.edu>
2014-04-18 12:20:35 -0700

    clean up

--- server/static/javascript/octal/collections/detailed-edge-collection.js
+++
@@ -1,6 +0,0 @@
-/*global define*/
-define(["backbone", "lib/kmapjs/collections/edge-collection", "octal/models/detailed-edge-model"], function(Backbone, EdgeCollection, DetailedEdgeModel){
-  return  EdgeCollection.extend({
-    model: DetailedEdgeModel
-  });
-});
