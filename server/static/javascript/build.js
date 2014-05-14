9611e1b479c36947ef90c81d95022196bfe60e11
Dan Armendariz <danallan@cs.berkeley.edu>
2014-05-14 14:42:54 -0700

    fix library location refs

--- server/static/javascript/build.js
+++ server/static/javascript/build.js
@@ -4,9 +4,9 @@
   out: "main-built.js",
     paths: {
     jquery:"//code.jquery.com/jquery-2.1.0.min",
-    underscore: "lib/underscore",
-    backbone: "lib/backbone-min",
-    d3: "lib/d3",
+    underscore: "//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min",
+    backbone: "//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min",
+    d3: "//cdnjs.cloudflare.com/ajax/libs/d3/3.4.5/d3.min",
     "btouch": "lib/backbone.touch",
   },
   shim: {
